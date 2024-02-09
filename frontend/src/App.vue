32b3b7bf05a8d467965b70fc4513f247cd0bb73c
Dwiky van Bosstraten <dwikyvanbosstraten@livewallgroup.com>
2024-02-09 17:18:11 +0100

    setup

---
+++ frontend/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <RouterView />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.page {
+  min-height: 100vh;
+  height: 100%;
+}
+</style>
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="page home">
+    <div class="full-width">
+      <div class="content-container">
+        <div class="content-holder">
+          <img class="logo" src="@/assets/images/logos/vcha-logo.png" alt="" />
+          <h1>WHICH VCHA MEMBER ARE YOU?</h1>
+        </div>
+        <div class="members-image"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HomeView',
+});
+</script>
+
+<style lang="scss" scoped>
+.page.home {
+  background-color: var(--black);
+  height: 100vh;
+
+  h1 {
+    color: white;
+    letter-spacing: 5px;
+  }
+
+  .full-width {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    background-color: red;
+
+    .content-container {
+      width: 100%;
+      height: 100%;
+      display: flex;
+      .content-holder {
+        background-color: rgb(194, 96, 250);
+        width: 40%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+      .members-image {
+        height: 100%;
+        width: 60%;
+        background-color: yellow;
+        background-image: url('@/assets/images/vcha-members.webp');
+        background-position: center center;
+        background-size: cover;
+      }
+    }
+
+    .logo {
+      max-width: 350px;
+    }
+  }
+}
+</style>
