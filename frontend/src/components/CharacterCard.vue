9a8b605b2330e02b79a9de34694975150c946ac3
Dwiky van Bosstraten <dwikyvanbosstraten@livewallgroup.com>
2024-02-24 00:10:39 +0100

    more card styling

--- frontend/src/components/CharacterCard.vue
+++ frontend/src/components/CharacterCard.vue
@@ -3,9 +3,26 @@
     <div class="member-image-container">
       <div class="member-image" :style="`background-image: url(/src/assets/images/members/${memberData?.member_img})`">
         <div class="top-card">
-          <p class="member-name">{{ memberData.name }}</p>
+          <div class="member-intro">
+            <p>{{ memberData.name }}</p>
+            <p>{{ memberData.birthday }}</p>
+          </div>
           <div class="number-circle">
             <p class="member-number">{{ memberData.favorite_number }}</p>
+          </div>
+        </div>
+        <div class="bottom-card">
+          <div class="member-basic">
+            <p>Favorite Season</p>
+            <p>{{ memberData.favorite_season }}</p>
+          </div>
+          <div class="member-basic">
+            <p>Special skills</p>
+            <p>{{ memberData.skills }}</p>
+          </div>
+          <div class="member-basic">
+            <p>Birth city</p>
+            <p>{{ memberData.birth_city }}</p>
           </div>
         </div>
       </div>
@@ -32,9 +49,8 @@ export default defineComponent({
 .card-container {
   width: 22vw;
   height: 65vh;
-  border-radius: 10px;
   background-image: url('@/assets/images/spark.gif');
-  box-shadow: inset 0 0 0 2000px rgb(57 76 147 / 70%);
+  box-shadow: inset 0 0 0 2000px rgb(74 36 101 / 60%);
 
   .member-image-container {
     height: 100%;
@@ -45,21 +61,28 @@ export default defineComponent({
       background-position: center;
       height: 100%;
       display: flex;
-      justify-content: center;
+      justify-content: space-between;
+      flex-direction: column;
+
       .top-card {
         display: flex;
         justify-content: center;
-        height: 12%;
         align-items: center;
-        width: 100%;
         justify-content: space-between;
-        padding: 35px 35px 0 35px;
+        height: 20%;
+        width: 100%;
+        padding: 0 30px 0 30px;
 
-        .member-name {
+        .member-intro {
           color: white;
           font-family: 'Abril Fatface';
-          font-weight: 100;
-          font-size: 3rem;
+          p:first-child {
+            font-size: 3rem;
+          }
+
+          p:nth-child(2) {
+            font-size: 1.2rem;
+          }
         }
 
         .number-circle {
@@ -76,6 +99,31 @@ export default defineComponent({
             color: rgb(80, 60, 128);
             font-family: 'Abril Fatface';
             font-size: 1.8rem;
+          }
+        }
+      }
+
+      .bottom-card {
+        height: 50%;
+        background: rgb(69,65,92);
+        background: linear-gradient(180deg, rgba(69,65,92,0) 0%, rgba(73,47,116,1) 100%);
+        padding: 35px;
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        justify-content: flex-end;
+
+        .member-basic {
+          p:first-child {
+            color: white;
+            font-family: 'Poppins';
+            font-size: 0.8rem;
+          }
+          p:nth-child(2) {
+            color: white;
+            font-family: 'Poppins';
+            font-weight: 700;
+            font-size: 1.1rem;
           }
         }
       }
